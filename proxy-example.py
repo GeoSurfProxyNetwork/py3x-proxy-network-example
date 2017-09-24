4ca7215caa7db392771e5b3802caa048e4c4b9c2
GeoSurf Residential IPs Proxy Network <32237331+GeoSurfProxyNetwork@users.noreply.github.com>
2017-09-24 14:02:04 +0300

    Update proxy-example.py

--- proxy-example.py
+++ proxy-example.py
@@ -7,10 +7,9 @@ import urllib.request as req
 # Our gateway’s hosthname + port, check your dashboard for full gateways list
 gs_proxy_addr = ‘gw1.geosurf.io:8081’
 
-proxy = req.ProxyHandler({
-‘http’: gs_proxy_addr,
-‘https’: gs_proxy_addr
-})
+proxy = req.ProxyHandler({‘http’: gs_proxy_addr,
+                          ‘https’: gs_proxy_addr})
+
 opener = req.build_opener(proxy)
 req.install_opener(opener)
 
